06b1f89f0ebb8056f8169bf1eacd9da0157bca03
mike-petrov <mis5116@yandex.ru>
2021-02-07 13:38:40 +0300

    fix web

--- web/src/App.js
+++ web/src/App.js
@@ -1,49 +1,156 @@
 import React, { useEffect, useState } from 'react';
+import {
+	withRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
 import './App.css';
+
+const qrcode = require('qrcode-generator');
 
 const App = () => {
 	const [userCredentials, setUserCredentials] = useState([]);
+	const [eventData, setEventData] = useState({
+		title: '',
+		description: '',
+		price: '',
+		link: '',
+	});
+	const [redirect, setRedirect] = useState({
+		status: false,
+		path: '/',
+	});
 
 	useEffect(() => {
-    // setUserCredentials();
+		if (localStorage.getItem('eventData')) {
+			const eventDataTemp = JSON.parse(localStorage.getItem('eventData'));
+			setEventData(eventDataTemp);
+		}
 	}, []);
 
+	const setInput = (e, type) => {
+    setEventData({ ...eventData, [type]: e.target.value });
+  };
+
+	const createEvent = () => {
+		// eventData
+		localStorage.setItem('eventData', JSON.stringify(eventData));
+
+		const linkHash = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+		setEventData({
+			title: '',
+			description: '',
+			price: '',
+			link: `${document.location.origin}/event/${linkHash.substr(0, 18)}`,
+		});
+  };
+
+	const buyTicket = () => {
+		console.log('!');
+  };
+
+	const sellTicket = () => {
+		console.log('!');
+  };
+	
+	const checkIn = () => {
+		onRedirect(`/qr/${document.location.pathname.split('/')[2]}`);
+
+		const qr = qrcode(4, 'L');
+		qr.addData(`/qr/${document.location.pathname.split('/')[2]}`);
+		qr.make();
+		setTimeout(() => {
+			document.getElementById('qr-code').innerHTML = qr.createImgTag();
+		}, 200);
+  };
+
+	const onRedirect = (_path) => {
+		setRedirect({ status: true, path: _path });
+	}
+
 	return (
-		<div>
 			<div className='content'>
 				<div className='title'>Welcome to Hedera Events</div>
-				<div className='events_list'>
-          <div className='event_block'>
-            <div className='event_header'>
-              <div className='event_title'>Hello Tokenization Welcome Webinar</div>
-              <div className='event_subtitle'>Welcome to the Hedera21 Tokenization hackathon! For our first webinar, we'll be joined by Hedera's Developer Evangelist Cooper Kunz. Cooper will be providing an overview of the hackathon.</div>
-            </div>
-            <div className='event_bottom'>
-              <div className='btn'>Take part</div>
-            </div>
-          </div>
-          <div className='event_block'>
-            <div className='event_header'>
-              <div className='event_title'>Deep Dive into Sponsor Challenges</div>
-              <div className='event_subtitle'>For this webinar, we'll be joined by all the Hedera21 challenge sponsors who will be doing a deep dive into their challenge and will provide examples of the types of applications and programs they are looking for.</div>
-            </div>
-            <div className='event_bottom'>
-              <div className='btn'>Take part</div>
-            </div>
-          </div>
-          <div className='event_block'>
-            <div className='event_header'>
-              <div className='event_title'>Deep Dive into DragonGlass</div>
-              <div className='event_subtitle'>For this webinar, we'll be joined by Ashu Mahajan, Director of Product Management, and Shawn Traynor, Head of Solutions at OpenCrowd. They will be giving you a deep dive on DragonGlass, a developer tool that allows you to access high quality historical and real-time data on the Hedera Hashgraph network.</div>
-            </div>
-            <div className='event_bottom'>
-              <div className='btn'>Take part</div>
-            </div>
-          </div>
-				</div>
+				<Switch>
+					{redirect.status === true && (
+						<>
+							<Redirect to={redirect.path} />
+							{setRedirect({ ...redirect, status: false })}
+						</>
+					)}
+					<Route exact path="/">
+						<div className='events_list'>
+							<div className='event_block'>
+								<div className='event_header'>
+									<div className='event_title'>New event</div>
+									<div className='event_form'>
+										<input
+											type="text"
+											value={eventData.title}
+											onChange={(e) => setInput(e, 'title')}
+											placeholder="Title"
+										/>
+										<textarea
+											type="text"
+											value={eventData.description}
+											onChange={(e) => setInput(e, 'description')}
+											placeholder="Description"
+										/>
+										<input
+											type="number"
+											value={eventData.price}
+											onChange={(e) => setInput(e, 'price')}
+											placeholder="Price (HBAR)"
+										/>
+									</div>
+									{eventData.link && (
+										<div className='event_subtitle'>{`Your event link: ${eventData.link}`}</div>
+									)}
+								</div>
+								<div className='event_bottom'>
+									<div className='btn' onClick={createEvent}>Create</div>
+								</div>
+							</div>
+						</div>
+					</Route>
+					<Route path="/event/:linkId">
+						<div className='events_list'>
+							<div className='event_block'>
+								<div className='event_header'>
+									<div className='event_title'>{eventData.title}</div>
+									<div className='event_subtitle'>{eventData.description}</div>
+									<div className='event_subtitle'>{eventData.price}</div>
+								</div>
+								<div className='event_bottom'>
+									{false ? (
+										<>
+											<div className='btn' onClick={sellTicket}>Sell</div>
+											<div className='btn' onClick={checkIn}>Check in</div>
+										</>
+									) : (
+										<div className='btn' onClick={buyTicket}>Buy</div>
+									)}
+								</div>
+							</div>
+						</div>
+					</Route>
+					<Route path="/qr/:linkId">
+						<div className='events_list'>
+							<div className='event_block'>
+								<div className='event_header'>
+									<div className='event_title'>{eventData.title}</div>
+									<div className='event_subtitle'>{eventData.description}</div>
+									<div className='event_subtitle'>{eventData.price}</div>
+								</div>
+								<div className='event_bottom'>
+									<div id="qr-code" />
+								</div>
+							</div>
+						</div>
+					</Route>
+					<Redirect to="/" />
+				</Switch>
+				<div className='copyright'>Hello future</div>
 			</div>
-			<div className='copyright'>Hello future</div>
-		</div>
 	);
 };
 
