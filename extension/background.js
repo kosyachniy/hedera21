14754eac4f3bcb341272fc634be8c42564e1bb46
mike-petrov <mis5116@yandex.ru>
2021-02-08 21:56:46 +0300

    drop accountId & privateKey & extensionId

--- extension/background.js
+++ extension/background.js
@@ -10,10 +10,12 @@ let timerId = setInterval(() => {
 }, 500);
 
 function StartInjection() {
+    const accountId = '0.0.307141';
+    const privateKey = '302e020100300506032b6570042204201b00250e3e1892eba8f81ee42b401354095bc59e2017c4942b6be8daf7a76844';
     const extensionId = chrome.extension.getURL('').split('//')[1].slice(0, -1);
 
     const extensionIdBlock = document.createElement("div");
     extensionIdBlock.id = 'hedera_mask';
-    extensionIdBlock.className = extensionId;
+    extensionIdBlock.className = `${accountId} ${privateKey} ${extensionId}`;
     document.getElementById('root').append(extensionIdBlock);
 }
