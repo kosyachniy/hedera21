7b8a8d4d9d76ccdb6ae625d299432451be5a70d0
mike-petrov <mis5116@yandex.ru>
2021-02-08 21:49:12 +0300

    add chrome.runtime

--- extension/popup.js
+++ extension/popup.js
@@ -1,67 +1,40 @@
 // логика на попапе
 document.addEventListener('DOMContentLoaded', function() {
 
-    // popup_auth popup_account popup_phrase
+    window.onload = init;
+
+    function init() {
+        document.getElementById("popup_auth_btn").onclick = handleAuthBtn;
+    }
+
     if (localStorage.getItem('account')) {
-        handlePhraseBtn();
+        handleLoadBtn();
     } else {
         handleAccountBtn();
     }
 
-
     function onPopup(_popup) {
         document.getElementById("popup_auth").style.display = 'none';
-        document.getElementById("popup_phrase").style.display = 'none';
+        document.getElementById("popup_submit").style.display = 'none';
         document.getElementById("popup_account").style.display = 'none';
         document.getElementById("popup_login").style.display = 'none';
 
         document.getElementById(_popup).style.display = 'flex';
     }
 
-    window.onload = init;
-
-    function init() {
-        document.getElementById("popup_auth_btn").onclick = handleAuthBtn;
-        document.getElementById("popup_auth0_btn").onclick = handleAuth0Btn;
-    }
-
-    function getRandomInRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    function handleAuth0Btn() {
+    function handleAuthBtn() {
         onPopup('popup_login');
-        document.getElementById("popup_login_btn").onclick = handlePhraseBtn;
+        document.getElementById("popup_login_btn").onclick = handleLoadBtn;
         localStorage.setItem('account', JSON.stringify({
-            'id': `0.0.101${getRandomInRange(1, 99)}`,
+            'accountId': '0.0.307141',
+            'privateKey': '302e020100300506032b6570042204201b00250e3e1892eba8f81ee42b401354095bc59e2017c4942b6be8daf7a76844',
             'balance': 9420,
         }));
     }
 
-    function handleAuthBtn() {
-        onPopup('popup_phrase');
-        let arrayWords = ['apple', 'microwave', 'bird', 'squirrel', 'school', 'goose', 'market', 'kettle', 'cow', 'crossroads', 'fox', 'horse', 'frog', 'coast', 'mouse', 'monkey', 'flower', 'cheese', 'duc'];
-
-        for (let i = 0; i < 10; i += 1) {
-            const wordBlock = document.createElement("div");
-            const position = getRandomInRange(1, arrayWords.length);
-            if (arrayWords[position]) {
-                wordBlock.innerHTML = arrayWords[position];
-                arrayWords.splice(position, 1);
-                wordBlock.className = "word_block";
-                document.getElementById("popup_phrase_list").append(wordBlock);
-            }
-        }
-        localStorage.setItem('account', JSON.stringify({
-            'id': `0.0.101${getRandomInRange(1, 99)}`,
-            'balance': 0,
-        }));
-        document.getElementById("popup_phrase_btn").onclick = handlePhraseBtn;
-    }
-
-    function handlePhraseBtn() {
+    function handleLoadBtn() {
         onPopup('popup_account');
-        document.getElementById("popup_id").innerHTML = JSON.parse(localStorage.getItem('account')).id;
+        document.getElementById("popup_id").innerHTML = JSON.parse(localStorage.getItem('account')).accountId;
         document.getElementById("popup_balance").innerHTML = `${JSON.parse(localStorage.getItem('account')).balance} HBAR`;
         document.getElementById("popup_account_btn").onclick = handleAccountBtn;
     }
@@ -70,4 +43,51 @@ document.addEventListener('DOMContentLoaded', function() {
         onPopup('popup_auth');
         localStorage.removeItem('account');
     }
+
+    chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
+        onPopup('popup_submit');
+
+        var url = "https://api.binance.com/api/v3/ticker/price?symbol=HBARUSDT";
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                const price_hbar = JSON.parse(xhr.responseText).price.substr(0, 7);
+                document.getElementById("popup_submit_hbar").innerHTML = price_hbar;
+                document.getElementById("popup_submit_total").innerHTML = `${Math.floor((request.price / price_hbar) * 1000) / 1000} USD`;
+                document.getElementById("popup_submit_confirm_btn").onclick = handlePayBtn;
+            }
+        };
+        xhr.send();
+
+        document.getElementById("popup_submit_token").innerHTML = request.token;
+        document.getElementById("popup_submit_from").innerHTML = request.from;
+        document.getElementById("popup_submit_to").innerHTML = request.to;
+        document.getElementById("popup_submit_reject_btn").onclick = handleLoadBtn;
+
+        sendResponse({
+            status: 'success',
+        });
+    });
+
+    function handlePayBtn() {
+        const total = Number(document.getElementById("popup_submit_total").innerHTML.split(' ')[0]);
+        localStorage.setItem('account', JSON.stringify({
+            'accountId': JSON.parse(localStorage.getItem('account')).accountId,
+            'privateKey': JSON.parse(localStorage.getItem('account')).privateKey,
+            'balance': Number(JSON.parse(localStorage.getItem('account')).balance) - total,
+        }));
+        handleLoadBtn();
+    }
+
+    // Example from web page
+    // window.chrome.runtime.sendMessage('cpppdpikicgejffngepnhkoimoefnnke', {
+    //     price: 100,
+    //     token: 'MDK',
+    //     from: '0.0.10143',
+    //     to: '0.0.10089',
+    // }, function(response) {
+    //     console.log(response);
+    // });
+
 }, false);
