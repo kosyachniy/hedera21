9c2177d17af5e8375115a2a385ff06f66ae51239
mike-petrov <mis5116@yandex.ru>
2021-02-09 19:14:05 +0300

    fixes

--- web/src/App.js
+++ web/src/App.js
@@ -11,13 +11,12 @@ const App = () => {
 	const [userCredentials, setUserCredentials] = useState(null);
 	const [userTokens, setUserTokens] = useState(null);
 	const [isShowBuyTicketTip, setShowBuyTicketTip] = useState(false);
+	const [isShowCreateEventTip, setShowCreateEventTip] = useState(false);
 	const [eventData, setEventData] = useState({
 		title: '',
-		tokenName: '',
-		description: '',
 		count: '',
 		price: '',
-		owner: '0.0.10315',
+		owner: '',
 		link: '',
 	});
 	const [redirect, setRedirect] = useState({
@@ -29,15 +28,15 @@ const App = () => {
 		if (localStorage.getItem('eventData')) {
 			const eventDataTemp = JSON.parse(localStorage.getItem('eventData'));
 			setEventData(eventDataTemp);
-
-			const loadUserCredentials = setInterval(() => {
-				if (document.getElementById('hedera_mask')) {
-					clearInterval(loadUserCredentials);
-					const [ accountId, privateKey, extensionId ] = document.getElementById('hedera_mask').className.split(' ')
-					setUserCredentials({ accountId, privateKey, extensionId });
-				}
-			}, 500);
 		}
+
+		const loadUserCredentials = setInterval(() => {
+			if (document.getElementById('hedera_mask')) {
+				clearInterval(loadUserCredentials);
+				const [ accountId, privateKey, extensionId ] = document.getElementById('hedera_mask').className.split(' ')
+				setUserCredentials({ accountId, privateKey, extensionId });
+			}
+		}, 500);
 	}, []);
 
 	useEffect(() => {
@@ -48,7 +47,7 @@ const App = () => {
 		} else if (userCredentials && document.location.pathname.indexOf('qr') !== -1) {
 			setTimeout(() => {
 				const qr = qrcode(4, 'L');
-				qr.addData(`/qr/${document.location.pathname.split('/')[2]}`);
+				qr.addData(`https://testnet.dragonglass.me/hedera/transactions/${document.location.pathname.split('/')[2]}`);
 				qr.make();
 				document.getElementById('qr-code').innerHTML = qr.createImgTag();
 			}, 200);
@@ -60,22 +59,39 @@ const App = () => {
   };
 
 	const createEvent = () => {
-		createToken(eventData.title, eventData.tokenName.toUpperCase()).then((token) => {
-			console.log('!createToken', true, token);
+		setShowCreateEventTip(true);
+		const sendTransaction = setInterval(() => {
+			window.chrome.runtime.sendMessage(userCredentials.extensionId, {
+	        title: eventData.title,
+	        count: eventData.count,
+	        priceTicket: eventData.price,
+	        from: userCredentials.accountId,
+	        to: '0.0.4',
+	    }, function(response) {
+				if (response) {
+					setShowCreateEventTip(false);
+					clearInterval(sendTransaction);
+					if (response.status === 'success') {
+						createToken(eventData.title, eventData.title.substr(0,5).toUpperCase()).then((token) => {
+							console.log('!createToken', token);
 
-			eventData.token = token
-			localStorage.setItem('eventData', JSON.stringify(eventData));
+							eventData.token = token;
+							localStorage.setItem('eventData', JSON.stringify(eventData));
 
-			setEventData({
-				title: '',
-				tokenName: '',
-				description: '',
-				count: '',
-				price: '',
-				owner: '0.0.10315',
-				link: `${document.location.origin}/event/${eventData.tokenName.toUpperCase()}`,
-			});
-		});
+							setEventData({
+								title: '',
+								count: '',
+								price: '',
+								owner: '',
+								link: `${document.location.origin}/event/${eventData.token}`,
+							});
+						});
+					} else {
+						console.log('!hederaMaskResponse', response);
+					}
+				}
+	    });
+		}, 500);
   };
 
 	const buyTicket = () => {
@@ -87,32 +103,38 @@ const App = () => {
 	        from: userCredentials.accountId,
 	        to: eventData.owner,
 	    }, function(response) {
-	        if (response && response.status === 'success') {
-						clearInterval(sendTransaction);
-
-						setTimeout(() => {
-							setShowBuyTicketTip(false);
-							buyToken(eventData.token, 1, userCredentials.accountId, userCredentials.privateKey).then((res) => {
-								console.log('!buyToken', true);
-								getBalance(userCredentials.accountId).then((result) => {
-									console.log('!getBalance', result);
-									setTimeout(() => {
-										document.location.reload();
-									}, 2000);
-								});
+				if (response) {
+					setShowBuyTicketTip(false);
+					clearInterval(sendTransaction);
+					if (response.status === 'success') {
+						buyToken(eventData.token, 1, userCredentials.accountId, userCredentials.privateKey).then((res) => {
+							console.log('!buyToken', true);
+							getBalance(userCredentials.accountId).then((result) => {
+								console.log('!getBalance', result);
+								setTimeout(() => {
+									document.location.reload();
+								}, 2000);
 							});
-						}, 7000);
+						});
+					} else {
+						console.log('!hederaMaskResponse', response);
 					}
+				}
 	    });
 		}, 500);
   };
 
 	const sellTicket = () => {
-		console.log('!');
+		console.log('!sellTicket');
+  };
+
+	const transferTicket = () => {
+		console.log('!transferTicket');
   };
 
 	const checkIn = () => {
-		document.location.href = `${document.location.origin}/qr/${document.location.pathname.split('/')[2]}`
+		const transactionId = '0.0.10313@1612880079.565634335';
+		document.location.href = `${document.location.origin}/qr/${transactionId.replaceAll('.','').replaceAll('@','')}`
   };
 
 	const onRedirect = (_path) => {
@@ -121,7 +143,6 @@ const App = () => {
 
 	return (
 			<div className='content'>
-				<div className='title'>Welcome to Hedera Events</div>
 				<Switch>
 					{redirect.status === true && (
 						<>
@@ -130,65 +151,80 @@ const App = () => {
 						</>
 					)}
 					<Route exact path="/">
+						<div className='title'>Hedera Smart Tickets</div>
+						<div className='subtitle'>Organiser</div>
 						<div className='events_list'>
 							<div className='event_block'>
-								<div className='event_header'>
-									<div className='event_title'>New event</div>
-									<div className='event_form'>
-										<input
-											type="text"
-											value={eventData.title}
-											onChange={(e) => setInput(e, 'title')}
-											placeholder="Title"
-										/>
-										<input
-											type="text"
-											value={eventData.tokenName}
-											onChange={(e) => setInput(e, 'tokenName')}
-											placeholder="Short name"
-											maxLength="5"
-										/>
-										<textarea
-											type="text"
-											value={eventData.description}
-											onChange={(e) => setInput(e, 'description')}
-											placeholder="Description"
-										/>
-										<input
-											type="number"
-											value={eventData.count}
-											onChange={(e) => setInput(e, 'count')}
-											placeholder="Number of tickets"
-										/>
-										<input
-											type="number"
-											value={eventData.price}
-											onChange={(e) => setInput(e, 'price')}
-											placeholder="Price"
-										/>
+								{!eventData.link ? (
+									<>
+										<div className='event_header'>
+											<div className='event_title'>New event</div>
+												<div className='event_form'>
+													<input
+														type="text"
+														value={eventData.title}
+														onChange={(e) => setInput(e, 'title')}
+														placeholder="Title"
+													/>
+													<input
+														type="number"
+														value={eventData.count}
+														onChange={(e) => setInput(e, 'count')}
+														placeholder="Number of tickets"
+													/>
+													<input
+														type="number"
+														value={eventData.price}
+														onChange={(e) => setInput(e, 'price')}
+														placeholder="Ticket price (HBAR)"
+													/>
+												</div>
+										</div>
+										{userCredentials ? (
+											<div className='event_bottom'>
+												{isShowCreateEventTip ? (
+													<div className='event_subtitle' style={{ color: '#3d7eeb' }}>Confirm transaction using HederaMask!</div>
+												) : (
+													<div className='btn' onClick={createEvent}>Issue tickets</div>
+												)}
+											</div>
+										) : (
+											<>
+												<div className='event_bottom'>
+													<div className='btn btn_disabled' disabled>Issue tickets</div>
+												</div>
+												<div className='event_subtitle' style={{ color: '#3d7eeb', marginTop: 30 }}>Cannot find HederaMask!</div>
+											</>
+										)}
+									</>
+								) : (
+									<div className='event_header'>
+										<div className='event_title'>Success</div>
+										<div className='event_subtitle'>Link to buy tickets:</div>
+										<div className='event_subtitle'>{eventData.link}</div>
 									</div>
-									{eventData.link && (
-										<div className='event_subtitle'>{`Your event link: ${eventData.link}`}</div>
-									)}
-								</div>
-								<div className='event_bottom'>
-									<div className='btn' onClick={createEvent}>Create</div>
-								</div>
+								)}
 							</div>
 						</div>
 					</Route>
 					<Route path="/event/:linkId">
+						<div className='title'>Hedera Smart Tickets</div>
+						{userTokens && Object.keys(userTokens).indexOf(eventData.token) !== -1 ? (
+							<div className='subtitle'>Your ticket</div>
+						) : (
+							<div className='subtitle'>Buy ticket</div>
+						)}
 						<div className='events_list'>
 							<div className='event_block'>
 								<div className='event_header'>
 									<div className='event_title'>{eventData.title}</div>
-									<div className='event_subtitle'>{eventData.description}</div>
-									<div className='event_subtitle'>{eventData.price}</div>
+									<div className='event_subtitle'>{`${eventData.price} HBAR`}</div>
 								</div>
 								{userCredentials && userTokens ? (
 									<div className='event_bottom'>
 										{Object.keys(userTokens).indexOf(eventData.token) !== -1 ? (
 											<>
+												<div className='btn' onClick={transferTicket}>Transfer</div>
 												<div className='btn' onClick={sellTicket}>Sell</div>
 												<div className='btn' onClick={checkIn}>Check in</div>
 											</>
@@ -203,22 +239,26 @@ const App = () => {
 										)}
 									</div>
 								) : (
-									<div className='event_bottom'>
-										<div className='btn btn_disabled' disabled>Buy</div>
-									</div>
+									<>
+										<div className='event_bottom'>
+											<div className='btn btn_disabled' disabled>Buy</div>
+										</div>
+										<div className='event_subtitle' style={{ color: '#3d7eeb', marginTop: 30 }}>Cannot find HederaMask!</div>
+									</>
 								)}
 							</div>
 						</div>
 					</Route>
 					<Route path="/qr/:linkId">
+						<div className='title'>Hedera Smart Tickets</div>
+						<div className='subtitle'>Your ticket</div>
 						<div className='events_list'>
 							<div className='event_block'>
 								<div className='event_header'>
 									<div className='event_title'>{eventData.title}</div>
-									<div className='event_subtitle'>{eventData.description}</div>
-									<div className='event_subtitle'>{eventData.price}</div>
 								</div>
 								<div className='event_bottom'>
+									<div className='subtitle'>Show this QR-code to enter</div>
 									<div id="qr-code" />
 								</div>
 							</div>
